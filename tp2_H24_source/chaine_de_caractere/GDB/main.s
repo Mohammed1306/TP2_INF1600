131c5e4a2945eba23bfc1bd8fcb43c5d2cbb3c43
Mohammed1306 <mohammed.kallel.2004@gmail.com>
2024-03-06 12:34:09 -0500

    GDB done

--- tp2_H24_source/chaine_de_caractere/GDB/main.s
+++ tp2_H24_source/chaine_de_caractere/GDB/main.s
@@ -22,12 +22,12 @@ xor %ebx, %ebx
 setup:
 movl $strings, %esi     
 
-//Garde le premier charactère du string "Allo Hi" dans %al
+//Charge un charactère du string "Allo Hi" à la fois dans %al
 n_c:
 lodsb                  
 
 //Cette étiquette vérifie que le charactère contenu dans %al est une lettre alphabétique.
-//Si c'est le cas, on saute à l'étiquette a
+//Si c'est le cas, on saute à l'étiquette a, sinon on charge le prochain charactère
 d:              
 cmp $0x20, %al
 je n_c2
@@ -48,12 +48,8 @@ n_c2:
 lodsb
 
 d2:
-//
-cmp $0x00, %al
+cmp $0, %al
 jz r
-//ERREUR: pour pouvoir comparer si le charactère est un espace on doit le comparer avec 0x20
-cmp $0x20, %al
-je n_c2
 cmp $0x41, %al
 jb n_c2
 cmp $0x5A, %al
@@ -71,27 +67,29 @@ jmp n_c2
 r:
 movl %ecx, %eax
 
-//on doit forcer edx a 0
+//ERREUR: on doit forcer %edx a 0 pour pouvoir diviser
 movl $0x00, %edx
 
-div %ebx
-//ERRUR: le résultat doit être comparé à 1 pour savoir si l'un est plus grand que l'autre
+divl %ebx
 cmp $0x00, %eax
 //ERREUR: on devrait sauter à e_n si le résultat de comparaison est vrai et à e_d si ce n'est pas le cas et pas l'inverse
-//ERREUR: on devrait sauter si le résultat est supérieur à 1 et non égal
 jz e_n
-jmp e_d              
+jmp e_d     
              
 //Affiche le numérateur
 e_n:
 push $num
 call printf
+//ERREUR: on doit incrémenter le %esp puisqu'on a push un paramettre
+addl $4, %esp
 
 jmp bye
 
 e_d:
 push $den
 call printf
+//ERREUR: on doit incrémenter le %esp puisqu'on a push un paramettre (je ne trouve pas de quatrième erreur et le code fonctionne quand même donc j'imagine que c'est celle la)
+addl $4, %esp
 
 
 bye:
