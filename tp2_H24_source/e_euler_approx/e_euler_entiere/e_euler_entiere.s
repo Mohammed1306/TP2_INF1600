6bf7e43766546637d3a5143852fb6f55c3ad3b84
jewak <144275912+jewak@users.noreply.github.com>
2024-03-08 12:51:38 -0500

    Update e_euler_entiere.s

--- tp2_H24_source/e_euler_approx/e_euler_entiere/e_euler_entiere.s
+++ tp2_H24_source/e_euler_approx/e_euler_entiere/e_euler_entiere.s
@@ -1,15 +1,75 @@
+.data
+un:
+.int 1
+iteration:
+.int 0
+n:
+.int 1
+valeur_e:
+.int 0
+denominateur:
+.int 1
+
 .text
 .globl e_euler_entiere
 
 e_euler_entiere:
+    push %ebp
+    mov %esp, %ebp
+    push %ebx
 
- 
-push %ebp
-mov %esp,%ebp
-push %ebx
+    xor %eax, %eax
+    xor %ebx, %ebx
+    xor %ecx, %ecx
+    xor %edx, %edx
 
-##VOTRE CODE ICI
+    movl 8(%ebp), %ebx
+    movl %ebx, iteration 
 
-pop %ebx
-pop %ebp
-ret
+    //# ajouter 1 pour n=0 (1/0!=1)
+    mov valeur_e,%ecx
+    add un, %ecx
+    mov %ecx,valeur_e
+
+Verifier_iteration_zero:
+    mov iteration,%ebx
+    cmp $0, %ebx
+    je fin
+
+denominateure:
+//3.2.2 Après le 12e itération, la valeur du dénominateur devient incorrecte 
+//( la valeur du denominateur(n!) attendu lorsque nombre d'itration=13 est 6227020800, cependant la valeur du denominatuer dans le registre %eax est 1932053504)
+    mov n, %eax
+    mov denominateur, %ecx
+    mul %ecx
+    mov %eax, denominateur
+    mov n, %eax
+    add un, %eax
+    mov %eax, n
+
+    // Division
+    mov un , %eax
+    mov denominateur, %ecx
+    div %ecx
+    
+    // Mise à jour de iteration
+    mov iteration, %ebx
+    sub un, %ebx
+    mov %ebx, iteration
+
+    jmp Sommation
+
+
+
+Sommation: 
+    add %eax, valeur_e
+    jmp Verifier_iteration_zero 
+
+fin:
+    
+    mov valeur_e, %eax
+    subl $8, %ebp
+    pop %ebx
+    pop %ebp
+    ret
+
