d02af48de752ef14658be6c27ece20be6a57758b
jewak <144275912+jewak@users.noreply.github.com>
2024-03-08 12:36:01 -0500

    Update e_euler_flottante.s

--- tp2_H24_source/e_euler_approx/e_euler_flottante/e_euler_flottante.s
+++ tp2_H24_source/e_euler_approx/e_euler_flottante/e_euler_flottante.s
@@ -22,8 +22,66 @@ push %ebp
 mov %esp,%ebp
 push %ebx
 
-## VOTRE CODE ICI 
+xor %ebx,%ebx 
 
-pop %ebx
-pop %ebp
-ret
+//# ajouter 1 pour n=0 (1/0!=1)
+flds e
+flds one
+faddp
+fstps e
+//ajouter 1 a n 
+flds factorial
+flds one
+faddp
+fstps factorial
+// ajouter 1 a n!
+flds factorial_value
+flds one
+faddp
+fstps factorial_value
+
+Verifier_iteration_zero:
+    mov iteration,%eax
+    cmp $0, %eax
+    je fin
+
+denominateure:
+
+    flds factorial
+    flds factorial_value
+    fmulp
+    fstps factorial_value
+    flds factorial
+    flds one
+    faddp
+    fstps factorial
+
+    // Division
+    flds factorial_value
+    flds one
+    fdivp
+    fstps reset
+    
+    // Mise à jour de iteration
+    sub $1,%eax
+    mov %eax, iteration
+    jmp Sommation
+
+
+
+Sommation: 
+    flds reset
+    flds e
+    faddp
+    fstps e
+    
+    jmp Verifier_iteration_zero 
+
+fin:
+    
+    flds e
+    pop %ebx
+    pop %ebp
+    ret
+
+
